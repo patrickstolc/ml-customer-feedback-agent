b2e32ff769bf72b64a68c7eff7b1ffcbd0a92cb9
Patrick Stolc <patrickstolc@gmail.com>
2025-04-07 12:47:33 +0200

    Update config.py

--- feedback_agent/config.py
+++ feedback_agent/config.py
@@ -2,13 +2,9 @@ LLM_CONFIG = {
     "model": "llama3.1:8b",
     "client_host": "127.0.0.1:11434",
     "api_type": "ollama",
-    "num_predict": -1,
     "repeat_penalty": 1.1,
     "stream": False,
     "seed": 42,
-    "temperature": 1,
-    "top_k": 50,
-    "top_p": 0.8,
     "native_tool_calls": False,
     "cache_seed": None,
 }
